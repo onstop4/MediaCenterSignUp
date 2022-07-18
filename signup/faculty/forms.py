6bdf16ffa0917d0af8c40fb3337ee89c33431656
Andrew Henkel <search1230@gmail.com>
2022-07-18 12:10:53 -0400

    Altered max student form to use HTML5 date picker

--- signup/faculty/forms.py
+++ signup/faculty/forms.py
@@ -2,12 +2,21 @@ from django import forms
 from django.conf import settings
 
 
+# See https://stackoverflow.com/a/22846522 for more info.
+class DateInput(forms.DateInput):
+    """Forces Django to render form using HTML5's date input field."""
+
+    input_type = "date"
+
+
 class FutureClassPeriodsForm(forms.Form):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         initial = kwargs.get("initial", {})
 
-        self.fields["date"] = forms.DateField(label="Date", initial=initial.get("date"))
+        self.fields["date"] = forms.DateField(
+            label="Date", initial=initial.get("date"), widget=DateInput
+        )
 
         for number in range(1, settings.MAX_PERIOD_NUMBER + 1):
             self.fields[f"period_{number}"] = forms.IntegerField(
