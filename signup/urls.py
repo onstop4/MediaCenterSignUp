b82789694edff85a83727015ce37ee49a50ac995
Andrew Henkel <search1230@gmail.com>
2022-07-21 20:37:34 -0400

    Added logout button to faculty views

--- signup/urls.py
+++ signup/urls.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.views import logout_then_login
 from django.urls import include, path
 
 from signup.views import (
@@ -11,6 +12,7 @@ from signup.views import (
 urlpatterns = [
     path("", index, name="index"),
     path("callback/", google_callback, name="google_callback"),
+    path("logout/", logout_then_login, name="logout"),
     path("s/", StudentSignUpFormView.as_view(), name="student_sign_up_form"),
     path("s/info/", StudentInfoFormView.as_view(), name="student_info_form"),
     path(
